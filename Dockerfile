f79e5587edea3a52c0ea50f2bd504f8976142751
cristianovisk <cristianovisk@gmail.com>
2023-06-19 17:24:44 -0300

    fix

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ COPY . .
 RUN /sbin/apk update
 RUN /sbin/apk add python3 py3-pip
 RUN /usr/bin/pip3 install -r requirements.txt
-CMD /usr/bin/python3 /app/run.py
+CMD /usr/bin/python3 /app/goat.py
--- goat.py
+++ goat.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI, HTTPException
+import io
+from starlette.responses import StreamingResponse
+import uuid
+import os
+import uvicorn
+import requests
 import subprocess
 import giturlparse
 
@@ -25,6 +31,34 @@ def hello_world(msg="hello_world"):
         return {"msg": "hello_world"}
     else:
         return {"msg": msg, "error": "not_hello_world"}
+    
+@app.get('/return_photo')
+def return_photo(filename):
+    files_types = ['jpg', 'png', 'svg']
+    if filename[-3:] == files_types[0] or filename[-3:] == files_types[1] or filename[-3:] == files_types[2]:
+        ext = filename[-3:]
+        with open(f'photos/{filename}', 'rb') as file:
+            return StreamingResponse(io.BytesIO(file.read()), media_type=f"image/{ext}")
+    else:
+        return {"status": "error", "msg": "invalid format file", "files_types": files_types}
+    
+@app.get('/upload_photo')
+def upload_photo(url):
+    files_types = ['jpg', 'png', 'svg']
+
+    if os.path.exists('photos') == False:
+        os.mkdir('photos')
+
+    if url[-3:] == files_types[0] or url[-3:] == files_types[1] or url[-3:] == files_types[2]:
+        ext = url[-3:]
+        random = uuid.uuid4()
+        
+        with open(f'photos/{random}.{ext}', 'wb') as file:
+            file.write(requests.get(url=url).content)
+
+        return {"status": "uploaded", "file_name": f"{random}.{ext}"}
+    else:
+        return {"status": "error", "msg": "invalid format file", "files_types": files_types}
 
 @app.get('/git_parse_url')
 def git_parse(git_url='git@github.com:retr0h/ansible-etcd.git'):
@@ -39,3 +73,6 @@ def git_parse(git_url='git@github.com:retr0h/ansible-etcd.git'):
         "resource": p.resource
     }
     return tmp
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8081, log_level="info", timeout_keep_alive=500, timeout_graceful_shutdown=500, ws_ping_timeout=500)
